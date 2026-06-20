06b63aa7281943d7385349886bd486b33d3d7748
j1-dev <aephyxen@gmail.com>
2026-06-20 13:05:41 +0200

    feat: add websocket handler lambda and dynamodb table with stream

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,107 @@
-terraform {                                                                        
-    required_providers {                                                             
-        aws = {                                                                        
-            source  = "hashicorp/aws"                                                    
-            version = "~> 5.0"                                                           
-        }                                                                              
-    }                                                                                
-}  
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
 provider "aws" {
-    access_key = "test"
-    secret_key = "test"
-    region     = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
 
-    s3_use_path_style           = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
-    endpoints {
-        dynamodb = "http://localhost:4566"
-    }
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    iam      = "http://localhost:4566"
+  }
 }
 
 resource "aws_dynamodb_table" "dynamodb" {
-    name = "the-wall"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "PK"
-    range_key = "SK"
+  name         = "the-wall"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
 
-    attribute {
-        name = "PK"
-        type = "S"
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "type"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "type-index"
+    hash_key        = "type"
+    projection_type = "ALL"
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+}
+
+data "archive_file" "lamda_zip" {
+  type        = "zip"
+  source_dir  = "../lambdas/websocket-handler"
+  output_path = "../lambdas/websocket-handler.zip"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
     }
 
-    attribute {
-        name = "SK"
-        type = "S"
-    }
+    actions = ["sts:AssumeRole"]
+  }
+}
 
-    attribute {
-        name = "type"
-        type = "S"
-    }
+data "aws_iam_policy_document" "websocket_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["dynamodb:PutItem", "dynamodb:DeleteItem"]
+    resources = [aws_dynamodb_table.dynamodb.arn]
+  }
+}
+resource "aws_iam_role" "websocket_handler_role" {
+  name               = "test_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
-    global_secondary_index {
-        name = "type-index"
-        hash_key = "type"
-        projection_type = "ALL"
-    }
+resource "aws_iam_role_policy" "websocket_handler_policy" {
+  name   = "websocket-handler-dynamodb-policy"
+  role   = aws_iam_role.websocket_handler_role.id
+  policy = data.aws_iam_policy_document.websocket_policy.json
+}
 
-    stream_enabled   = true
-    stream_view_type = "NEW_IMAGE"
+resource "aws_lambda_function" "websocket_handler" {
+  runtime          = "nodejs22.x"
+  handler          = "dist/index.handler"
+  function_name    = "websocket-handler"
+  role             = aws_iam_role.websocket_handler_role.arn
+  filename         = data.archive_file.lamda_zip.output_path
+  source_code_hash = data.archive_file.lamda_zip.output_base64sha256
+  environment {
+    variables = {
+      TABLE_NAME = "the-wall"
+    }
+  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,11 @@
-output "dynamodb_table_arn" {                                                      
-    value = aws_dynamodb_table.dynamodb.arn
+output "dynamodb_table_arn" {
+  value = aws_dynamodb_table.dynamodb.arn
 }
-              
-output "dynamodb_stream_arn" {                                 
-    value = aws_dynamodb_table.dynamodb.stream_arn
+
+output "dynamodb_stream_arn" {
+  value = aws_dynamodb_table.dynamodb.stream_arn
 }
+
+output "websocket_handler_arn" {
+  value = aws_lambda_function.websocket_handler.arn
+} 
